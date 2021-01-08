3ff5b6d3508c2d50e2a176ed2d5f0f1c22afed4a
Evgenii Akentev <i@ak3n.com>
2021-01-09 01:29:03 +0500

    Fix backpack-handles example with two signatures

--- backpack-handles/impl/SuperWeatherReporter.hs
+++ backpack-handles/impl/SuperWeatherReporter.hs
@@ -1,25 +1,25 @@
 module SuperWeatherReporter where
 
-import qualified SuperWeatherProvider
+import qualified WeatherProvider
 
 type WeatherReport = String
 
-type WeatherProviderHandle = SuperWeatherProvider.Handle
+type WeatherProviderHandle = WeatherProvider.Handle
 
 -- | We hide dependencies in the handle
-data Handle = Handle { weatherProvider :: SuperWeatherProvider.Handle }
+data Handle = Handle { weatherProvider :: WeatherProvider.Handle }
 
 -- | Constructor for Handle
-new :: SuperWeatherProvider.Handle -> Handle
+new :: WeatherProvider.Handle -> Handle
 new = Handle
 
 -- | Domain logic. Usually some pure code that might use mtl, free monads, etc.
-createWeatherReport :: SuperWeatherProvider.WeatherData -> WeatherReport
-createWeatherReport (SuperWeatherProvider.WeatherData temp) =
+createWeatherReport :: WeatherProvider.WeatherData -> WeatherReport
+createWeatherReport (WeatherProvider.WeatherData temp) =
   "The current temperature in London is " ++ (show temp)
 
 -- | Domain logic that uses external dependency to get data and process it.
 getCurrentWeatherReportInLondon :: Handle -> IO WeatherReport
 getCurrentWeatherReportInLondon (Handle wph) = do
-  weatherData <- SuperWeatherProvider.getWeatherData wph "London" "now"
+  weatherData <- WeatherProvider.getWeatherData wph "London" "now"
   return $ createWeatherReport weatherData
